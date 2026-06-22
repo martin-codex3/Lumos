f592ca79eabf571f7bf65bc7a08bbd8300ca2b82
Martin Silungwe <martinsilungwe12@gmail.com>
2026-06-22 15:46:26 +0200

    changing the database session

--- Backend/app/core/database.py
+++ Backend/app/core/database.py
@@ -1,17 +1,15 @@
-from sqlmodel import create_engine
 from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.ext.asyncio.engine import create_async_engine
+from sqlalchemy.ext.asyncio.session import async_sessionmaker
+from sqlmodel.ext.asyncio.session import AsyncSession
 from app.core.config import Config
 from sqlmodel import SQLModel
-from sqlmodel import Session
-from sqlmodel.ext.asyncio.session import AsyncSession
 
 
 # we will create the database engine here 
-database_engine = AsyncEngine(
-    create_engine(
-        url = Config.DATABASE_URL,
-        echo = True
-    )
+database_engine: AsyncEngine = create_async_engine(
+    url = Config.DATABASE_URL,
+    echo = True
 )
 
 # this function will attempt to connect to the database here
@@ -21,11 +19,16 @@ async def init_database():
         await connection.run_sync(SQLModel.metadata.create_all)
 
 async def close_database_connect():
-    async with database_engine.begin() as connection:
-        connection.close
+    await database_engine.dispose()
 
 # we will create the database session here 
 # this will be used for the dependency injection
+database_session = async_sessionmaker(
+    bind=database_engine,
+    class_=AsyncSession,
+    expire_on_commit = False
+)
+
 async def get_database_session():
-    with Session(bind=database_engine) as session:
+    async with database_session() as session:
         yield session
